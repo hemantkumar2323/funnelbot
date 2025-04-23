48c3f5538d92bb441a08e2b681c31f8731839918
hemantkumar2323 <freelanceb23@gmail.com>
2025-04-23 16:50:24 +0530

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FunnelBot from './FunnelBot.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FunnelBot />
+  </React.StrictMode>
+)
